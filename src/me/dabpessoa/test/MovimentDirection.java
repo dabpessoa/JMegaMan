e27f74b48c7d37a9519091f8a6033c5e8f138117
Diego Pessoa <dabpessoa@gmail.com>
2017-01-17 12:05:23 -0300

    melhoria algoritmo de colisão.

--- src/me/dabpessoa/test/MovimentDirection.java
+++ src/me/dabpessoa/test/MovimentDirection.java
@@ -16,6 +16,70 @@ public enum MovimentDirection {
     RIGHT_TO_LEFT_AND_TOP_TO_BOTTOM,
     NO_MOVIMENT;
 
+    public boolean isGoingToRight() {
+        return isGoingToRight(this);
+    }
+
+    public boolean isGoingToLeft() {
+        return isGoingToLeft(this);
+    }
+
+    public boolean isGoingToBottom() {
+        return isGoingToBottom(this);
+    }
+
+    public boolean isGointToTop() {
+        return isGointToTop(this);
+    }
+
+    public boolean isGoingToTopRight() {
+        return isGoingToTopRight(this);
+    }
+
+    public boolean isGoingToTopLeft() {
+        return isGoingToTopLeft(this);
+    }
+
+    public boolean isGoingToBottomRight() {
+        return isGoingToBottomRight(this);
+    }
+
+    public boolean isGoingToBottomLeft() {
+        return isGoingToBottomLeft(this);
+    }
+
+    public static boolean isGoingToRight(MovimentDirection movimentDirection) {
+        return movimentDirection == LEFT_TO_RIGHT || isGoingToBottomRight(movimentDirection) || isGoingToTopRight(movimentDirection);
+    }
+
+    public static boolean isGoingToLeft(MovimentDirection movimentDirection) {
+        return movimentDirection == RIGHT_TO_LEFT || isGoingToBottomLeft(movimentDirection) || isGoingToTopLeft(movimentDirection);
+    }
+
+    public static boolean isGoingToBottom(MovimentDirection movimentDirection) {
+        return movimentDirection == TOP_TO_BOTTOM || isGoingToBottomRight(movimentDirection) || isGoingToBottomLeft(movimentDirection);
+    }
+
+    public static boolean isGointToTop(MovimentDirection movimentDirection) {
+        return movimentDirection == BOTTOM_TO_TOP || isGoingToTopLeft(movimentDirection) || isGoingToTopRight(movimentDirection);
+    }
+
+    public static boolean isGoingToTopRight(MovimentDirection movimentDirection) {
+        return movimentDirection == LEFT_TO_RIGHT_AND_BOTTOM_TO_TOP;
+    }
+
+    public static boolean isGoingToTopLeft(MovimentDirection movimentDirection) {
+        return movimentDirection == RIGHT_TO_LEFT_AND_BOTTOM_TO_TOP;
+    }
+
+    public static boolean isGoingToBottomRight(MovimentDirection movimentDirection) {
+        return movimentDirection == LEFT_TO_RIGHT_AND_TOP_TO_BOTTOM;
+    }
+
+    public static boolean isGoingToBottomLeft(MovimentDirection movimentDirection) {
+        return movimentDirection == RIGHT_TO_LEFT_AND_TOP_TO_BOTTOM;
+    }
+
     public static MovimentDirection findMovimentDirection(Point oldPoint, Point newPoint) {
         MovimentDirection movimentDirection = null;
         if (oldPoint.x < newPoint.x) {
